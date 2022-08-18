e5c08c4202a44c8151604c60ac73daf352c66421
Kevin Savill <103211354+ksavill@users.noreply.github.com>
2022-08-18 00:44:29 -0500

    8-18-2022 1
    Added Driver Implicit Wait function that can be adjusted in realtime

--- SelectorTester.py
+++ SelectorTester.py
@@ -19,6 +19,17 @@ class Menus:
                 dpg.add_button(label="Start new driver instance",callback=start_driver)
                 dpg.add_text("")
 
+                #these fields do not do anything yet.
+                with dpg.group(horizontal=True):
+                    dpg.add_text("Implicit Wait? ")
+                    dpg.add_checkbox(tag="driverImplicitWait",default_value=True)
+                with dpg.group(horizontal=True):
+                    dpg.add_text("Implicit Wait Time (seconds):")
+                    dpg.add_input_int(tag="driverImplicitWaitTime",default_value=3,width=100,on_enter=True,callback=setImplicitWait)
+                dpg.add_text("Once turned on, it cannot be turned off until new driver instance is made.")
+                dpg.add_button(label="Update Implicit Wait Time",callback=setImplicitWait)
+
+                dpg.add_text("")
                 #field for telling the driver what URL to go to to start
                 dpg.add_text("Target URL")
                 with dpg.group(horizontal=True):
@@ -48,9 +59,7 @@ class Menus:
                 with dpg.group(horizontal=True):
                     dpg.add_text("Selector Path: ")
                     dpg.add_input_text(tag="selectorPath")
-                dpg.add_text("")
                 dpg.add_button(label="Perform Action",callback=Testing.selectorTest)
-                dpg.add_text("")
                 dpg.add_text("---Result---")
                 with dpg.group(horizontal=True):
                     dpg.add_text("Selector Action Test Result: ")
@@ -93,8 +102,21 @@ class Testing:
 def start_driver():
     global driver
     driver = webdriver.Chrome(r'ChromeDriver/chromedriver.exe')
-    driver.implicitly_wait(3)
+    setImplicitWait()
     return driver
+
+def setImplicitWait():
+    global driver
+    if dpg.get_value("driverImplicitWait") == False:
+        return
+    implicitWaitTime = dpg.get_value("driverImplicitWaitTime")
+    if implicitWaitTime < 0:
+        print("invalid input for implicit wait time, skipping function.")
+        return
+    else:
+        driver.implicitly_wait(implicitWaitTime)
+        print("driver set to have implicit wait of " + str(implicitWaitTime) + " seconds.")
+        return driver
 
 """modified sel class for selector path testing. The main changes will be that sys.press_enter() will be removed and will be returning true or false for a result variable
 based on if an exception was raised."""
